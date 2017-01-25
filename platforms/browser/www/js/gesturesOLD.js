c6e149cfe9dfe1add44261aab6fa7e9a933ff3c6
Jonnathan Berbesi <jberbesil@gmail.com>
2017-01-24 23:20:38 -0400

    Ejercicio de Gesture con PhoneGap

---
+++ platforms/browser/www/js/gesturesOLD.js
@@ -0,0 +1,26 @@
+var app={
+	inicio: function(){
+		var botonClaro  = document.querySelector("#claro");
+		var botonOscuro = document.querySelector("#oscuro");
+
+		botonClaro.addEventListener("click", this.ponloClaro,false);
+		botonOscuro.addEventListener("click", this.ponloOscuro,false);
+	},
+
+	ponloClaro: function(){
+		document.body.className = "claro";
+	},
+
+	ponloOscuro: function(){
+		document.body.className = "oscuro";
+	},
+};
+
+//app.inicio();
+
+if ("addEventListener" in document) {
+	document.addEventListener("DOMConten tLoaded", function(){
+		FastClick.attach(document.body);
+		app.inicio();
+	}, false)
+}
